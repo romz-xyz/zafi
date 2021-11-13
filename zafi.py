2a04d7ab7ddecabadc6226cc2a49ba1d5c02838c
bot-85 <94216075+bot-85@users.noreply.github.com>
2021-11-13 23:21:22 +0700

    Update zafi.py

--- zafi.py
+++ zafi.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python2
 # coding=utf-8
-# coding by Romi Afrizal
+# coding: Bintang XD
 # Note : jangan di ubah lagi! nanti error, script udah enak
 # Open source code team | ngotak dikit cok jangan jual di perjual belikan 
 
@@ -62,7 +62,7 @@ def folder():
 IP = requests.get("https://api.ipify.org/").text
 def banner():
 	print (''' %s 
- © Group%s \n __________       _____.__  \n \____    /____ _/ ____\__| %s> %sZona \n   /     /\\__  \\\   __\|  | %s> %sAkun \n  /     /_ / __ \|  |  |  | %s>%s Facebook [Meta] \n /_______ (____  /__|  |__| %s>%s Indonesia \n         \/    \/ \n %s[%s*%s] By : %sRomi Afrizal \n %s[%s*%s] -------------------------------------- \n [%s*%s] IP : %s%s'''%
+_______  _______  _______  _______ _________          _______sF\x00\x00\x00\x1b[1;91m(  ____ )(  ____ )(  ____ \\(       )\\__   __/|\\     /|(       )sF\x00\x00\x00\x1b[1;91m| (    )|| (    )|| (    \\/| () () |   ) (   | )   ( || () () |sF\x00\x00\x00\x1b[1;91m| (____)|| (____)|| (__    | || || |   | |   | |   | || || || |sF\x00\x00\x00\x1b[1;97m|  _____)|     __)|  __)   | |(_)| |   | |   | |   | || |(_)| |sF\x00\x00\x00\x1b[1;97m| (      | (\\ (   | (      | |   | |   | |   | |   | || |   | |sF\x00\x00\x00\x1b[1;97m| )      | ) \\ \\__| (____/\\| )   ( |___) (___| (___) || )   ( |sF\x00\x00\x00\x1b[1;97m|/       |/   \\__/(_______/|/     \\|\\_______/(_______)|/     \\ By : %sBintang XD \n %s[%s*%s] -------------------------------------- \n [%s*%s] IP : %s%s'''%
  (H,K,H,K,H,K,H,K,H,K,P,K,P,H,P,K,P,K,P,H,IP))
 # MASUK TOKEN (TOKEN LISTRIK)
 header = {"x-fb-connection-bandwidth": str(random.randint(20000000.0, 30000000.0)), "x-fb-sim-hni": str(random.randint(20000, 40000)), "x-fb-net-hni": str(random.randint(20000, 40000)), "x-fb-connection-quality": "EXCELLENT", "x-fb-connection-type": "cell.CTRadioAccessTechnologyHSDPA", "user-agent": "NokiaC3-00/5.0 (07.20) Profile/MIDP-2.1 Configuration/CLDC-1.1 Mozilla/5.0 AppleWebKit/420+ (KHTML, like Gecko) Safari/420+ ;]", "content-type": "application/x-www-form-urlencoded", "x-fb-http-engine": "Liger"}
